fd726befd4bf2f1c342d5e189e31df0ad2266d1c
stesix <7610155+stesix@users.noreply.github.com>
2018-01-24 13:54:41 +0100

    Update scrypt-async definition to version 1.3.1

--- types/scrypt-async/index.d.ts
+++ types/scrypt-async/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for scrypt-async v1.2.0
+// Type definitions for scrypt-async v1.3.1
 // Project: https://github.com/dchest/scrypt-async-js
 // Definitions by: Kaur Kuut <https://github.com/xStrom>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -9,7 +9,22 @@ declare namespace ScryptAsync {
 		(key: number[]): any;
 	}
 
+	interface Options {
+		N?: number;
+		logN?: number;
+		r: number;
+		p: number;
+		dkLen: number;
+		encoding?: string;
+		interruptStep?: number;
+	}
+
 	interface ScryptStatic {
+		(password: string, salt: string, options: Options, callback: CallbackFunc): void;
+		(password: number[], salt: string, options: Options, callback: CallbackFunc): void;
+		(password: string, salt: number[], options: Options, callback: CallbackFunc): void;
+		(password: number[], salt: number[], options: Options, callback: CallbackFunc): void;
+		
 		(password: string, salt: string, logN: number, r: number, dkLen: number, interruptStep: number, callback: CallbackFunc, encoding: string): void;
 		(password: string, salt: number[], logN: number, r: number, dkLen: number, interruptStep: number, callback: CallbackFunc, encoding: string): void;
 		(password: number[], salt: string, logN: number, r: number, dkLen: number, interruptStep: number, callback: CallbackFunc, encoding: string): void;
